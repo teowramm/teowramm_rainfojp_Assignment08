6529f128bdc1ac1095df3cc8b859fa6d671303ae
teowramm <teowramm@vdi-LCB-IC-012.ad.uc.edu>
2024-03-21 16:43:34 -0400

    fourth commit (created list in connect.py)

--- teowramm_rainfojp_Assignment08/ConnectPackage/Connect.py
+++ teowramm_rainfojp_Assignment08/ConnectPackage/Connect.py
@@ -1,6 +1,5 @@
 
-
-
+    
 import pyodbc 
 conn = pyodbc.connect('Driver={SQL Server};'
                       'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
@@ -10,11 +9,22 @@ conn = pyodbc.connect('Driver={SQL Server};'
 
 cursor = conn.cursor()
 
-cursor.execute('SELECT') # edit this to put code inside parentheses (SELECT, FROM, WHERE)
+cursor.execute('SELECT [GroceryStoreSimulator].[dbo].[tStore].[StoreID], [GroceryStoreSimulator].[dbo].[tStoreStatus].[StoreStatus], COUNT([GroceryStoreSimulator].[dbo].[tLoyalty].[LoyaltyID]) AS NmLoyal FROM [GroceryStoreSimulator].[dbo].[tStoreHistory] INNER JOIN [GroceryStoreSimulator].[dbo].[tStore] ON [GroceryStoreSimulator].[dbo].[tStore].[StoreID] = [GroceryStoreSimulator].[dbo].[tStoreHistory].[StoreID] INNER JOIN [GroceryStoreSimulator].[dbo].[tStoreStatus] ON [GroceryStoreSimulator].[dbo].[tStoreHistory].[StoreStatusID] = [GroceryStoreSimulator].[dbo].[tStoreStatus].[StoreStatusID] INNER JOIN [GroceryStoreSimulator].[dbo].[tLoyalty] ON [GroceryStoreSimulator].[dbo].[tStoreHistory].[StoreID] = [GroceryStoreSimulator].[dbo].[tLoyalty].[StoreID] GROUP BY [GroceryStoreSimulator].[dbo].[tStore].[StoreID], [GroceryStoreSimulator].[dbo].[tStoreStatus].[StoreStatus] ORDER BY [GroceryStoreSimulator].[dbo].[tStore].[StoreID]') # edit this to put code inside parentheses (SELECT, FROM, WHERE)
 
-loyaltyList = list()
+
+
+    
+storestatusList = list()
+
+
 
 for row in cursor:
-    if row.Loyalty ==3 # edit this
-    loyaltyList.append(row.Loyalty.rstrip(" "))
-print(loyaltyList)
+    if row.StoreStatus == "On Fire                                                                                             " and row.StoreID ==2:
+        storestatusList.append((row.NmLoyal))
+print(storestatusList) # number of loyalty customers that the store with storeID =2 lost when they closed due to the fire
+
+
+
+
+
+
