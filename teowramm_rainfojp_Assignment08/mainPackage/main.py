ca9d6a5ed094c6e053cf31c8f0854390efd66645
teowramm <teowramm@vdi-LCB-IC-002.ad.uc.edu>
2024-03-21 13:19:57 -0400

    Second Commit

--- teowramm_rainfojp_Assignment08/mainPackage/main.py
+++ teowramm_rainfojp_Assignment08/mainPackage/main.py
@@ -1,12 +1,2 @@
 # main.py
 
-import pyodbc 
-conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=lcb-sql.uccob.uc.edu\\nicholdw;'
-                      'Database=IS4010;'
-                      'uid=IS4010Login;'
-                      'pwd=P@ssword2;')
-
-cursor = conn.cursor()
-
-cursor.execute() # put code inside parentheses (SELECT, FROM, WHERE)
